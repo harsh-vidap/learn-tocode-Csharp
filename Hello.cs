92187bfac34efb58d24f94daf98ae8bcd70f2065
Harshavardhan Vidap <harshavardhan@ryoshiga.co.uk>
2021-06-24 11:13:20 +0530

    second commit

---
+++ Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] str)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
